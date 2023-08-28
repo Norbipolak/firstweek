2f214f57cf8ba2b6c9168ae35b1750cd7e41f5ef
Norbipolak <142392455+Norbipolak@users.noreply.github.com>
2023-08-28 08:38:41 +0200

    practise

---
+++ age.js
@@ -0,0 +1,16 @@
+let myFirstFiveThings = ["artificial intelligence", "punk", "Javascript", "solar system", "growth mindset"]
+
+console.log(myFirstFiveThings)
+
+let myBestFriends = [
+    {
+        name: "David"
+        age: 30
+    },
+    {
+        name: "Tibor"
+        age: 30
+    }
+]
+
+console.log(myBestFriends) // got an error message for age 
---
+++ monday.js
@@ -0,0 +1,20 @@
+let MyName = "Norbert"
+MyName = "Norbi"
+
+let myName = "Norbert"
+let myNewName = "Norbi"
+myName = myNewName
+
+console.log(myName)
+
+let myAge = 19
+let myNewAge = myAge
+myAge = 18
+
+console.log(myAge)
+
+let myOldAge = 18
+let myNewAge = myOldAge + 1 
+
+console.log(myNewAge)
+
